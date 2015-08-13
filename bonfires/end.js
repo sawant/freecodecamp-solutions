089835599e2aeb058fd87164d25d40502d94adc8
Sawant Shah <github@sawantshah.com>
2015-08-13 14:45:47 +0500

    Delete end.js

--- bonfires/end.js
+++
@@ -1,7 +0,0 @@
-function end(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-  return str.substr(str.length - target.length, target.length) === target;
-}
-
-end('Bastian', 'n');
